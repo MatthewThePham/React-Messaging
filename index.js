6548697b2812eca0c33fa1d1e1f1d48795fe3be3
matto475@gmail.com <matto475@gmail.com>
2019-09-11 02:08:00 -0700

    Added heroku code

--- index.js
+++ index.js
@@ -98,6 +98,15 @@ io.on('connection', function (socket) {
 
 });
 
+//for production aka heroku
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static('client/build'));
+
+  app.get('*', (req,res) => {
+      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  })
+}
+
 const port = process.env.PORT || 5000;
 
 server.listen(port, function() {
